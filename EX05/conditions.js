327008e824c1e22d2c87fb5eae78b9423795ace3
Vinicius Ruan Fillos <107576816+ViniciusFillos@users.noreply.github.com>
2023-12-29 15:25:46 -0300

    aula11

---
+++ EX05/conditions.js
@@ -0,0 +1,14 @@
+var vel = 77
+
+console.log(`A velocidade do seu carro é ${vel} Km/h`)
+
+if(vel>60) {
+    console.log(`Você ultrapassou a Velocidade permitida em ${(vel-60)} Km/h, MULTADO!`)
+
+} else {
+    console.log(`Você esta a ${(vel-60)*-1} Km/h do limite`)
+}
+
+console.log(`Dirija com segurança! `)
+
+
