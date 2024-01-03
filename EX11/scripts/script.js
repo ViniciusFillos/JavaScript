99aa9500d079b87f7cda9a7e5f7e9575e46b071e
Vinicius Ruan Fillos <107576816+ViniciusFillos@users.noreply.github.com>
2024-01-03 14:01:58 -0300

    exercicio horas do dia

---
+++ EX11/scripts/script.js
@@ -0,0 +1,24 @@
+
+function carregar() {
+    var msg = window.document.querySelector('div#msg')
+    var img = window.document.getElementById('imagem')
+    var DateToday = new Date()
+    var hora = 133//DateToday.getHours()
+    msg.innerHTML = `Agora são ${hora} horas`
+
+    if (hora<13) {
+        //BOM DIA
+        img.src="images/morning.png"
+        document.body.style.background = 'var(--cor08)'
+    } else if(hora<19) {
+        //BOA TARDE
+        img.src = "images/afternoon.png"
+        document.body.style.background = 'var(--cor07)'
+    } else {
+        //BOA NOITE
+        img.scr ='images/evening.png'
+        document.body.style.background = 'var(--cor06)'
+
+    }
+}
+
