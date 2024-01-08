aca43d2bf0bab200975f0d59489630663a66329a
TinkyyWinki <107576816+ViniciusFillos@users.noreply.github.com>
2024-01-08 20:35:34 -0300

    tabuada

--- EX15/scripts/script.js
+++ EX15/scripts/script.js
@@ -1,1 +1,19 @@
-alert('Olá')
+function tabuada () {
+
+    let num = document.querySelector('#idnum')
+    let res = document.querySelector('#res')
+
+
+    if (num.value.length == 0) {    
+        alert(`Por favor digite um número!`)
+    } else {
+        let n = Number(num.value)
+        res.innerHTML = ``
+        for (i = 1; i < 11; i ++) {
+            let item = document.createElement('option')
+            item.value = `tab${i}`
+            item.text = `${n} X ${i} = ${i*n}`
+            res.appendChild(item)
+        }
+    }
+}
