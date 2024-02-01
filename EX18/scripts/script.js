cfe36e0c929d239273453455b79ad5300c2fe829
TinkyyWinki <107576816+ViniciusFillos@users.noreply.github.com>
2024-01-31 22:00:58 -0300

    completed

--- EX18/scripts/script.js
+++ EX18/scripts/script.js
@@ -23,9 +23,41 @@ function inList(n, l) {
 
 function add() {
     if (isNumero(num.value) && !inList(num.value, values)) {
-        
+        values.push(Number(num.value))
+        let item = document.createElement('option')
+        item.text = `Valor ${num.value} adicionado.`
+        list.appendChild(item)
+        res.innerHTML = ``
     } else {
         alert('Valor inválido ou já encontrado na listax')
+    }
+    num.value = ``
+    num.focus()
+}
+
+function finalize() {
+    if (values.length == 0) {
+        window.alert(`Adicione valores antes de finalizar`)
+    } else {
+        let tot = values.length
+        let max = values[0]
+        let min = values[0]
+        let sum = 0
+        for (let pos in values) {
+            if (values[pos] > max) {
+                max = values[pos]
+            }
+            if (values[pos] < min) {
+                min = values[pos]
+            }
+            sum += values[pos]
+        }
+        res.innerHTML = ``
+        res.innerHTML += `<p> Ao todo, temos ${tot} números cadastrados.</p>`
+        res.innerHTML += `<p> O maior valor informado foi ${max}</p>`
+        res.innerHTML += `<p> O menor valor informado foi ${min}</p>`
+        res.innerHTML += `<p> A soma dos valores é de ${sum}`
+        res.innerHTML += `<p> A média dos valores é de ${sum/tot}`
     }
 }
 
